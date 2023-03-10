ee1a23043360cc17f254691444f75c4cd433c27b
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-10 12:36:08 -0800

    add Client.cs, HairSalonContext.cs, Stylist.cs, connect the database and build out splash page

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+namespace HairSalon.Controllers;
+
+
+public class HomeController : Controller
+{
+  [Route("/")]
+  public ActionResult Index() 
+  {
+    return View();
+  }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+namespace HairSalon.Models;
+
+  public class Stylist
+  {
+    public int StylistId {get;set;}
+    public string StylistName {get;set;}
+    public List<Client> Clients {get;set;}
+  }
